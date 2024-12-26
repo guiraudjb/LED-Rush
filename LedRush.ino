17c798c7aead8ec16f4d240d86a33631d62bdbee
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2024-12-26 21:54:17 +0100

    Update LedRush.ino

--- LedRush.ino
+++ LedRush.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
-#include <Lycidcrystal_I2C.h>
+#include <LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x27,20,4);
 
 int LED5V = 2;
 int boutonP1 = 4; // Numero du PIN du bouton 1
@@ -18,10 +19,176 @@ int timer;
 int velocityP1, velocityP2;
 int oldPosP1, oldPosP2;
 
-int playerColors[2][3] = {
-  {0, 0, 255},
-  {0, 255, 0}
+
+uint8_t blank[] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000
 };
+
+uint8_t custChar[8][8] = {
+{
+  B11111,
+  B11111,
+  B11111,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000
+},// Small top line - 0
+
+{
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B11111,
+  B11111
+},// Small bottom line - 1
+
+
+{
+  B11111,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B11111,
+  B11111
+},// Small lines top and bottom -2
+
+
+{
+  B11111,
+  B11111,
+  B11111,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B11111
+}, // -3
+
+
+
+{
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B01111,
+  B00111
+},// Left bottom chamfer full - 4
+
+{
+  B11100,
+  B11110,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111
+},// Right top chamfer full -5
+
+
+{
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11110,
+  B11100
+},// Right bottom chamfer full -6
+
+{
+  B00111,
+  B01111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111
+},// Left top chamfer full -7
+ 
+};
+
+// Define our numbers 0 thru 9
+// 254 is blank and 255 is the "Full Block"
+uint8_t bigNums[38][6] = {
+  {7, 0, 5, 4, 1, 6}, //0
+  {0, 5, 254, 1, 255, 1},     //1
+  {0, 0, 5, 4, 2, 2},         //2
+  {3, 3, 5, 2, 2, 6},         //3
+  {4, 1, 5, 254, 254, 255},   //4
+  {255, 3, 3, 2, 2, 6},       //5 
+  {7, 3, 3, 4, 2, 6},         //6 
+  {0, 0, 5, 254, 7, 254},     //7 
+  {7, 3, 5, 4, 2, 6},         //8 
+  {7, 3, 5, 254, 254, 6},     //9
+  {0, 3, 5, 254, 2, 254},//? 10
+  {254, 255, 254, 254, 2, 254},//! 11 
+ {7, 3, 5, 255, 254, 255}, //A 12
+ {255, 3, 6, 255, 2, 5}, //B 13
+ {7, 0, 0, 4, 1, 1}, //C 14
+ {255, 0, 5, 255, 1, 6}, //D 15
+ {255, 3, 3, 255, 2, 2}, //E 16
+ {255, 3, 3, 255, 254, 254}, //F 17
+ {7, 0, 0, 4, 1, 5}, //G 18
+{255, 1, 255, 255, 254, 255},  //H 19
+ {0, 255, 0, 1, 255, 1}, //I 20
+ {254, 254, 255,1,1, 6}, //J 21
+ {255, 1, 6, 255, 254, 5}, //K 22
+ {255, 254, 254, 255, 1, 1}, //L 23
+ {5,1,7,255,254,255}, //M 24
+ {5,1,255,255,0,4}, //N 25
+ {7, 0, 5, 4, 1, 6}, //O 26
+ {255, 3, 5, 255, 254, 254}, //P 27
+ {7,0,5,4,1,255}, //Q 28
+ {255, 3, 5, 255, 254,5}, //R 29
+ {7, 3, 3, 2, 2,6}, //S 30
+ {0, 255, 0, 254, 255, 254}, //T 31
+ {255, 254, 255, 4, 1, 6}, //U 32
+ {5, 254, 7, 4, 1, 6}, //V 33
+ {5, 1, 7, 4,255, 6}, //W 34
+ {4, 1, 6, 7, 254, 5}, //X 35
+ {4,1,6,254,255,254}, //Y 36
+ {0,3,6,7,2,1}, //Z 37
+  
+};
+
+struct RGB {
+    int r;
+    int g;
+    int b;
+};
+
+RGB playerColor1 = {0, 0, 255};
+RGB playerColor2 = {0, 255, 0}
+
+RGB mixColors(const RGB& color1, const RGB& color2) {
+    RGB mixedColor;
+    mixedColor.r = (color1.r + color2.r) / 2;
+    mixedColor.g = (color1.g + color2.g) / 2;
+    mixedColor.b = (color1.b + color2.b) / 2;
+    return mixedColor;
+}
+
+RGB mixedColor = mixColors(playerColor1, playerColor2);
 
 void setup() {
   Serial.begin(9600);
@@ -33,7 +200,7 @@ void setup() {
 
   lcd.init();
   lcd.init();
-  lcd.setBri
+  lcd.backlight();
 
   pinMode(boutonP1, INPUT_PULLUP);
   pinMode(boutonP2, INPUT_PULLUP);
@@ -41,46 +208,52 @@ void setup() {
 }
 
 void loop() {
-  if (!digitalRead(boutonP1) && winning == false) {
-    velocityP1 += 1;
-  } else if (winning == true) {
-    Reset();
-  }
-  if (!digitalRead(boutonP2) && winning == false) {
-    if (clickP2 >= 3) {
-      clickP2 = 0;
-      velocityP2 += 3;
-    }
-    clickP2 += 1;
-  }
-
-  if (velocityP1 > 150) { velocityP1 = 0; tourP1 = tourP1 + 1; }
-  if (velocityP2 > 150) { velocityP2 = 0; tourP2 = tourP2 + 1; }
-
-  if (tourP1 == NombreTours) { EndGame(); }
-  if (tourP2 == NombreTours) { EndGame(); }
-
-  MillisTimer=millis();
-  if (MillisTimer - oldTimer > 1000) {
-    /*if (velocityP1 > 1) {
-      velocityP1 -= 1;
-    }*/
-    timer += 1;
-    oldTimer = MillisTimer;
-  }
-
   if (winning == false) {
-    AfficherTimer();
-    printBigNum(tourP1, 1, 1);
-    printBigNum(tourP2, 16, 1);
+    if (!digitalRead(boutonP1) && winning == false) {
+      velocityP1 += 1;
+    } else if (winning == true) {
+      Reset();
+    }
+    if (!digitalRead(boutonP2) && winning == false) {
+        velocityP2 += 1;
+    }
+
+    if (velocityP1 > 150) { velocityP1 = 0; tourP1 = tourP1 + 1; }
+    if (velocityP2 > 150) { velocityP2 = 0; tourP2 = tourP2 + 1; }
+
+    if (tourP1 == NombreTours) { EndGame(); }
+    if (tourP2 == NombreTours) { EndGame(); }
+
+    MillisTimer=millis();
+    if (MillisTimer - oldTimer > 1000) {
+      timer += 1;
+      oldTimer = MillisTimer;
+    }
+
+    if (timer < 60) {
+      AfficherTimer();
+      printBigNum(tourP1, 1, 1);
+      printBigNum(tourP2, 16, 1);
+    } else {
+      EndGame();
+    }
 
     pixels.clear();
 
-    pixels.setPixelColor(velocityP1, pixels.Color(playerColors[0][0], playerColors[0][1], playerColors[0][2]));
-    pixels.setPixelColor(velocityP1 + 1, pixels.Color(playerColors[0][0], playerColors[0][1], playerColors[0][2]));
+    pixels.setPixelColor(velocityP1, pixels.Color(playerColor1.r, playerColor1.g, playerColor1.b));
+    pixels.setPixelColor(velocityP1 + 1, pixels.Color(playerColor1.r, playerColor1.b, playerColor1.g));
 
-    pixels.setPixelColor(velocityP2, pixels.Color(playerColors[1][0], playerColors[1][1], playerColors[1][2]));
-    pixels.setPixelColor(velocityP2 + 1, pixels.Color(playerColors[1][0], playerColors[1][1], playerColors[1][2]));
+    pixels.setPixelColor(velocityP2, pixels.Color(playerColor2.r, playerColor2.g; playerColor2.b));
+    pixels.setPixelColor(velocityP2 + 1, pixels.Color(playerColor2.r, playerColor2.g, playerColor2.b));
+
+    if (velocityP1 == velocityP2) {
+      pixels.setPixelColor(velocityP1, pixels.Color(mixedColor.r, mixedColor.g, mixedColor.b));
+      pixels.setPixelColor(velocityP1 + 1, pixels.Color(mixedColor.r, mixedColor.g, mixedColor.b));
+    } else if (velocityP1 == velocityP2 - 1) {
+      pixels.setPixelColor(velocityP1 + 1, pixels.Color(mixedColor.r, mixedColor.g, mixedColor.b));
+    } else if (velocityP1 == velocityP2 - 1) {
+      pixels.setPixelColor(velocityP1, pixels.Color(mixedColor.r, mixedColor.g, mixedColor.b));
+    }
 
     pixels.show();
   } else {
@@ -122,6 +295,7 @@ void AfficherTimer(){
 }
 
 void EndGame() {
+  lcd.clear();
   winning = true;
   tourP1 = 0;
   tourP2 = 0;
